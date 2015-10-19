bf5e4cf9a051234ed05ec8e94f735ebf3b8d1b91
91 <hatelove@users.noreply.github.com>
2015-10-19 16:47:37 +0800

    Update TDD_DAY1_HOMEWORK_TEST.php
    整體來說寫得很好，建議的部分都在註解裡面，可以參考一下。

--- php/tests/TDD_DAY1_HOMEWORK_TEST.php
+++ php/tests/TDD_DAY1_HOMEWORK_TEST.php
@@ -7,6 +7,8 @@ class TDD_DAY1_HOMEWORK_TEST extends PHPUnit_Framework_TestCase{
 	
 	protected function setUp(){
 		$data = [];
+		// $data 的部分不建議這樣寫，因為 Cost, Revenue, SellPrice 的值，都只是剛好看起來是流水號，但實際上都是獨立的
+		// 因為就需求或 domain 的意義來說，沒有任何需求或是domain意義來支撐他們得是個連續數字。
 		for($i = 1;$i<=11;$i++){
 			array_push($data,['Id'=>$i,'Cost'=>$i,'Revenue'=>$i+10,'SellPrice'=>$i+20]);
 		}
@@ -14,12 +16,14 @@ class TDD_DAY1_HOMEWORK_TEST extends PHPUnit_Framework_TestCase{
 		$this->TDD_DAY1_HOMEWORK_TEST->setData($data);
 	}
 	
+	//寫得很好, getCustomData() 的 function name 如果更符合需求的意義就更好了。
 	public function test_Column_Name_Cost_3_Group(){
 		$expected = [6,15,24,21];
 		$actual = $this->TDD_DAY1_HOMEWORK_TEST->getCustomData('Cost', 3);
 		$this->assertEquals($expected,$actual);
 	}
 	
+	//寫得很好, getCustomData() 的 function name 如果更符合需求的意義就更好了。
 	public function test_Column_Name_Revenue_4_Group(){
 		$expected = [50,66,60];
 		$actual = $this->TDD_DAY1_HOMEWORK_TEST->getCustomData('Revenue', 4);
